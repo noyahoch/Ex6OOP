d9853aae4d847364153b96492e2f2d394df0a78f
omriisack <omriisack@gmail.com>
2018-06-09 20:04:12 +0300

    Factory

--- src/main/CommandFactory.java
+++ src/main/CommandFactory.java
@@ -2,18 +2,20 @@ package main;
 
 import block.*;
 import variable.*;
-
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class CommandFactory {
     ArrayList<Block> blocks;
     ArrayList<Variable> vars;
-    private final String validSuffixOnce = "[$;{}]{1}";
-    enum VarTypes {INT, DOUBLE, CHAR, BOOLEAN, String}
-    enum BlockTypes {WHILE, IF, VOID}
+    private final String validSuffixOnce = "$[;{}]";
 
+    private final String varType = "(final )?(int|double|String|char|boolean)";
+    private final String blockType = "while|if|void";
+    private final String END_BLOCK = "}";
+    private Block currentBlock = null;
 
     /**
      * Checks the if the lines in the file are valid.
@@ -24,15 +26,70 @@ public class CommandFactory {
         for (String line : lines){
             Pattern p = Pattern.compile(validSuffixOnce);
             Matcher m = p.matcher(line);
-            String firstWord = line.split(" ")[0];
             if (!m.matches())
                 return false;
-            } else if(firstWord.toUpperCase()== BlockTypes.values()){
+            p = Pattern.compile(varType);
+            m = p.matcher(line);
+            if (m.lookingAt()) {
+                boolean valid = createVars(line);
+                if (!valid)
+                    return false;
+                continue;
+            }
+            p = Pattern.compile(blockType);
+            m = p.matcher(line);
+            if (m.lookingAt()){
+                boolean valid = createBlock(line);
+                if (!valid)
+                    return false;
+                continue;
+            }
+            if (line == END_BLOCK){
+                if (currentBlock == null)
+                        return false;
+                currentBlock = currentBlock.getParent();
+            }
 
-            } else if (firstWord.){
 
-            } else {
-            
+
         }
+        for (Block block : blocks){
+            if (!block.checkValidity())
+                return false;
+        }
+        for (Variable var : vars){
+            if (!var.checkValidity())
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Reads a var declaration and creates Variable objects according to it.
+     * Adds the variables to the variable arraylist.
+     * @param line a line representing a variable declaration
+     * @returns true iff succeeded creating variable objects
+     */
+    private boolean createVars (String line){
+        boolean succeeded = false;
+        String[] words = line.split(" ");
+        String type = words[0];
+
+        return succeeded;
+    }
+
+
+    /**
+     * Reads a block declaration and creates Block objects according to it.
+     * Adds the blocks to the blocks arraylist.
+     * @param line a line representing a blocks declaration
+     * @returns true iff succeeded creating block objects
+     */
+    private boolean createBlock (String line){
+        boolean succeeded = false;
+
+
+        return succeeded;
+
     }
 }
