44b6cde58124954cf959dc4c5b405324754d5fed
omriisack <omriisack@gmail.com>
2018-06-14 21:13:59 +0300

    factoryyyy

--- src/block/BlockFactory.java
+++ src/block/BlockFactory.java
@@ -8,26 +8,27 @@ import java.util.ArrayList;
 import java.util.regex.*;
 
 public class BlockFactory {
-	ArrayList<Variable> parameters;
-<<<<<<< HEAD
-	CodeReader reader;
-=======
-<<<<<<< HEAD
-	public Block blockFactory(String type, String[]parameters) throws Exception {
-		if (type.equals("void")){
-			for (String parameter:parameters) {
-				CommandFactory.createVars (parameter);
-=======
->>>>>>> 7786052641f340bf53e7af120809275032b043bc
->>>>>>> b5745ac844185d00b0c09bb03d8f910c27578654
-
 	private static final String METHOD_DEC = "(void )" + Method.VALID_METHOD_NAME
 			+ "\\(([\\w ,]*)\\) *{ *";
 	private static final String CONDITIONAL = "(if|while)\\(([\\w \\|&]*\\)) *{ *";
+	private static final String METHOD_KEY = "void";
+	private static final String IF_KEY = "if";
+	private static final String WHILE_KEY = "while";
+	private static Pattern blockPattern;
+	private static Matcher blockMatcher;
 
-	public Block createBlock(String type, String line, CodeReader reader)  {
-		Pattern detectBlock = p.compile()
+	public static Block createBlock(String type, String line, CodeReader reader) throws IOException  {
+		if (type.contains(METHOD_KEY))
+			blockPattern = Pattern.compile(METHOD_DEC);
+		else
+			blockPattern = Pattern.compile(CONDITIONAL);
 
+		 blockMatcher = blockPattern.matcher(line);
+		if (!blockMatcher.matches())
+			throw new IOException ("ILLEGAL BLOCK CREATION");
+		if(type.contains(METHOD_KEY))
+			return createMethod(blockMatcher.group(2), reader);
+		return createConditional(blockMatcher.group(2), reader);
 	}
 
 
@@ -36,15 +37,16 @@ public class BlockFactory {
 	 * @param line the line to check
 	 * @throws IOException if line is invalid
 	 */
-	private static void createConditional (String line, CodeReader reader) throws IOException{
+	private static Block createConditional (String line, CodeReader reader) throws IOException{
 		Block decBlock = reader.getCurrentBlock();
 		if (decBlock == null)
 			throw new IOException("CANNOT DECLARE CONDITIONAL IN GLOBAL SCOPE");
 		try{
-			Block cond = new Conditional(m.group(2), decBlock);
+			Block cond = new Conditional(blockMatcher.group(2), decBlock);
 			boolean valid = cond.checkValidity();
 			if (!valid)
 				throw new IOException("INVALID BOOLEAN CONDITION");
+			return cond;
 		} catch (Exception e){
 			throw new IOException("INVALID IF WHILE DECLARATION");
 		}
@@ -59,25 +61,25 @@ public class BlockFactory {
 	 * @param line a line representing a method declaration
 	 * @throws IOException if line is invalid
 	 */
-	private static void createMethod(String line, Block defBlock) throws IOException {
+	private static Block createMethod(String line, CodeReader reader) throws IOException {
+		Block decBlock = reader.getCurrentBlock();
 		try{
-			if (defBlock.isMethod())
+			if (decBlock.isMethod())
 				throw new IOException("DECLARED METHOD WITHIN METHOD");
-			String methodName = m.group(2);
-			for (Method method : methods)
+			String methodName = blockMatcher.group(2);
+			for (Method method : reader.getMethods())
 				if (method.getName().equals(methodName))
 					throw new IOException("METHOD OVERLOADING IS NOT SUPPORTED");
 
 			ArrayList<Variable> params = new ArrayList<>();
-			String[] varDec = m.group(3).split(",");
+			String[] varDec = blockMatcher.group(3).split(",");
 			for (String declaration : varDec)
-				createVars(declaration, params);
+				reader.createVars(declaration, params);
 
-			Block createdMethod = new Method(methodName, defBlock, params);
-			defBlock = createdMethod;
-			methods.add(createdMethod);
+			Block createdMethod = new Method(methodName, decBlock, params);
 			if (!createdMethod.checkValidity())
 				throw new IOException("INVALID METHOD DECLARATION");
+			return createdMethod;
 		} catch (Exception e){
 			throw new IOException("INVALID METHOD DECLARATION");
 		}
